42e3cd88388d07195d0c097c63dab4da3eca77f6
Nicole Morales <moralesrobladillon@gmail.com>
2024-11-02 09:28:55 -0500

    Add endpoint to get orders by user id

--- app/infrastructure/repository/adapter/order_repository_adapter.py
+++ app/infrastructure/repository/adapter/order_repository_adapter.py
@@ -4,6 +4,7 @@ from app.infrastructure.repository.configuration.database_configuration import S
 from app.domain.model.order import Order
 from app.infrastructure.repository.model.order_entity import OrderEntity
 from sqlalchemy.exc import SQLAlchemyError
+from typing import List
 
 class OrderRepositoryAdapter:
     def __init__(self):
@@ -25,6 +26,26 @@ class OrderRepositoryAdapter:
             self.session.add(order_model)
             self.session.commit()
             return order_model
+        except SQLAlchemyError as e:
+            self.session.rollback()
+            raise e
+        finally:
+            self.session.close()
+
+    def get_orders_by_user(self, user_id: int) -> List[Order]:
+        try:
+            orders = self.session.query(OrderEntity).filter(OrderEntity.user_id == user_id).all()
+            return [Order(
+                user_id=order.user_id,
+                symbol=order.symbol,
+                sec_type=order.sec_type,
+                exchange=order.exchange,
+                quantity=order.quantity,
+                price=order.price,
+                status=order.status,
+                order_type=order.order_type,
+                order_id=order.order_id
+            ) for order in orders]
         except SQLAlchemyError as e:
             self.session.rollback()
             raise e
--- app/infrastructure/rest/controllers/order_controller.py
+++ app/infrastructure/rest/controllers/order_controller.py
@@ -1,16 +1,18 @@
 # app\infrastructure\rest\controllers\order_controller.py
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends, Query
 from app.application.service.order_service import OrderService
 from app.infrastructure.rest.model.buy_stock_request import BuyStockRequest
-from pydantic import constr
 from fastapi.responses import JSONResponse
 from app.infrastructure.configuration.dependencies import get_order_service
-from fastapi import Depends
+from datetime import date
 
 router = APIRouter()
 
-@router.post("/buy")
+@router.post(
+    "/buy", 
+    summary="Endpoint para ejecutar órdenes de compra de acciones.", 
+    description="Se ejecuta la orden de compra de acciones y se almacena la información en base de datos.")
 def buy_stock(request: BuyStockRequest, order_service: OrderService = Depends(get_order_service)):
     try:
         order = order_service.place_order(
@@ -22,6 +24,19 @@ def buy_stock(request: BuyStockRequest, order_service: OrderService = Depends(ge
             order_type=request.order_type,
             price=request.price
         )
-        return JSONResponse(content={"status": "success", "order": order.dict()}, status_code=201)
+        return JSONResponse(content=order.dict(), status_code=201)
     except Exception as e:
-        return JSONResponse(content={"status": "error", "detail": str(e)}, status_code=500)
+        return JSONResponse(content={"timestamp": date.today().isoformat(), "messages": [str(e)]}, status_code=500)
+
+@router.get("", summary="Endpoint para obtener órdenes por ID de un usuario.", 
+description="Se filtran las órdenes ejecutadas por un usuario en específico."
+)
+def get_orders_by_user(
+    user: int = Query(..., gt=0, description="ID del usuario del cual se extraen sus órdenes."),
+    order_service: OrderService = Depends(get_order_service)
+):
+    try:
+        orders = order_service.get_orders_by_user(user_id=user)
+        return JSONResponse(content=[order.dict() for order in orders], status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"timestamp": date.today().isoformat(), "messages": [str(e)]}, status_code=500)
